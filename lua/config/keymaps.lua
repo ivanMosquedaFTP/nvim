eb8754cd2ca48584b2f1958e8539c6aabfd0dcca
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-10 00:22:46 -0600

    added harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,12 @@ vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory
 
 -- transparent.nvim
 vim.keymap.set("n", "<leader>ut", "<CMD>TransparentToggle<CR>", { desc = "Toggle transparency" })
+
+-- harpoon.nvim
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add to harpoon" })
+vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon list" })
+vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end, { desc = "Harpoon prev" })
+vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end, { desc = "Harpoon next" })
