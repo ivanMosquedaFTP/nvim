922c93daafa7c3d3fc852fc4976287141323909c
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-18 22:54:09 -0600

    dropped support for blade framework

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -71,19 +71,6 @@ require("lspconfig").intelephense.setup({
   init_options = {},
 })
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade", -- local path or git repo
-    files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-    -- optional entries:
-    branch = "main", -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-  },
-  filetype = "blade", -- if filetype does not match the parser name
-}
-
 require("flutter-tools").setup({}) -- use defaults
 
 -- The setup config table shows all available config options with their default values:
